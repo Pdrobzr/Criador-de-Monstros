5d062513c35ed6812993e498d68bdf26d164f2a4
Pedro Bezerra <pedrohbs67@hotmail.com>
2023-06-02 13:54:16 -0300

    Update usuario

--- components/navbar.php
+++ components/navbar.php
@@ -79,7 +79,7 @@
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            <?=$dados['nm_usuario'];?>
+            <?=$dados['nm_usuario']?>
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Conta</a></li>
